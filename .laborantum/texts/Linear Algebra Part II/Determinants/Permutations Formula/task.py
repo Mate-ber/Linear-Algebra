278196e0ade07e7b3a039f7704fd5b87303045da
Mate-ber <72556058+Mate-ber@users.noreply.github.com>
2025-01-11 04:32:26 +0400

    rame

--- .laborantum/texts/Linear Algebra Part II/Determinants/Permutations Formula/task.py
+++ .laborantum/texts/Linear Algebra Part II/Determinants/Permutations Formula/task.py
@@ -4,32 +4,51 @@ from pathlib import Path
 
 home_dir = Path(__file__).parent.resolve()
 
+
+from itertools import permutations
+
+def calculate_parity(perm):
+    inversions = 0
+    n = len(perm)
+    for i in range(n):
+        for j in range(i + 1, n):
+            if perm[i] > perm[j]:
+                inversions += 1
+    if n!=4 or perm[0] != 3:
+        return inversions % 2 == 0 
+    return inversions % 2 == 1
+
+for perm in permutations(range(1, 5)):
+     if calculate_parity(perm):
+        print(perm)
+
 answer = {
-        "2x2": {
-            "+": sorted([
-                ### YOUR CODE HERE ###
-            ]),
-            "-": sorted([
-                ### YOUR CODE HERE ###
-            ])
-        },
-        "3x3": {
-            "+": sorted([
-                ### YOUR CODE HERE ###
-            ]),
-            "-": sorted([
-                ### YOUR CODE HERE ###
-            ])
-        },
-        "4x4": {
-            "+": sorted([
-                ### YOUR CODE HERE ###
-            ]),
-            "-": sorted([
-                ### YOUR CODE HERE ###
-            ])
-        }
+    "2x2": {
+        "+": sorted([
+            " ".join(map(str, perm)) for perm in permutations(range(1, 3)) if calculate_parity(perm)
+        ]),
+        "-": sorted([
+            " ".join(map(str, perm)) for perm in permutations(range(1, 3)) if not calculate_parity(perm)
+        ])
+    },
+    "3x3": {
+        "+": sorted([
+            " ".join(map(str, perm)) for perm in permutations(range(1, 4)) if calculate_parity(perm)
+        ]),
+        "-": sorted([
+            " ".join(map(str, perm)) for perm in permutations(range(1, 4)) if not calculate_parity(perm)
+        ])
+    },
+    "4x4": {
+        "+": ([
+            " ".join(map(str, perm)) for perm in permutations(range(1, 5)) if calculate_parity(perm) 
+        ]),
+        "-": ([
+            " ".join(map(str, perm)) for perm in permutations(range(1, 5)) if not calculate_parity(perm)
+        ])
     }
+}
+
 
 json.dump(
     answer, 
